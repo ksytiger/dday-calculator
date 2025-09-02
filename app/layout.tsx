66956526af49a71c826b3f673718c70bdeb87efa
ksytiger <sebalgang@gmail.com>
2025-09-02 10:19:07 +0900

    chore(favicon): add explicit 48x48, 32x32, 16x16 links in <head>

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
   },
 }
 
-export const viewport: Viewport = {
+export const viewport: Viewport = { 
   themeColor: "#EB5A0F",
   width: "device-width",
   initialScale: 1,
@@ -45,6 +45,7 @@ export default function RootLayout({
       <head>
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
